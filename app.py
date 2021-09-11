dfed4709bf181e02e7353617b44045e1c95a461c
Matheus Oliveira <teteuoliveira12@gmail.com>
2021-09-10 22:11:27 -0300

    OK

--- app.py
+++ app.py
@@ -9,16 +9,17 @@ def hello_world():  # put application's code here
     url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
     response = requests.get(url)
     data = []
-    for item in response.json():
+    for item, index in zip(response.json(), range(len(response.json()))):
         data.append({
-            "imagem" : item['image_link'],
-            "marca" : item['brand'],
-            "nome" : item['name'],
-            "preco" : item['price'] + " " + item['price_sign'] + " " + item['currency'],
-            "link" : item['product_link']
+            "id": index+1,
+            "imagem": item['image_link'],
+            "marca": item['brand'],
+            "nome": item['name'],
+            "preco": item['price'],
+            "link": item['product_link']
         })
     total = len(data)
-    return render_template('index.html', data=data, total=total)
+    return render_template('index.html', produtos=data, total=total)
 
 
 if __name__ == '__main__':
